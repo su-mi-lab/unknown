140731ab5b9787f4dbe9a8ddcd89369428975e6b
yuta.sumiyoshi <sumiyoshi102@gmail.com>
2018-05-08 16:18:24 +0900

    プロジェクト再出発

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # UnKnown
+
+## 初回設定
+
+### Umbrellaプロジェクトの生成
+
+```
+docker-compose run mix new unknown --umbrella
+```
---
+++ copy_docker-compose.yml.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cp -f docker-compose.yml apps/data_store/docker-compose.yml
+cp -f docker-compose.yml apps/service/docker-compose.yml
