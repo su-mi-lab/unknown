0e28479df69ee22e622fcf01dfe78a33ea1c82a0
yuta.sumiyoshi <sumiyoshi102@gmail.com>
2018-05-16 12:41:18 +0900

    Handlerのテストとdoc修正

--- apps/handler/lib/handler_behaviour.ex
+++ apps/handler/lib/handler_behaviour.ex
@@ -1,5 +1,7 @@
 defmodule HandlerBehaviour do
 
+  @moduledoc false
+
   @callback run(Map.t) :: Article.t
 
 end
--- apps/handler/test/handler/article_handler_test.exs
+++ apps/handler/test/handler/article_handler_test.exs
@@ -1,10 +1,9 @@
 defmodule Handler.ArticleHandlerTest do
   use DataStore.ModelCase
-  doctest Handler.ArticleHandler
 
   alias Handler.ArticleHandler
 
-  test "find by id" do
+  test "article_handler test" do
     article = ArticleHandler.run(%{"id" => Stub.Article.get_article_id()})
     assert article.title == "some title"
   end
