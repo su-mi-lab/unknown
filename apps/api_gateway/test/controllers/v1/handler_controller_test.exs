3dd60ad6d701dc214e07953e59112d5734caaf1e
yuta.sumiyoshi <sumiyoshi102@gmail.com>
2018-06-26 10:25:49 +0900

    テストコードをきれいにした

--- apps/api_gateway/test/controllers/v1/handler_controller_test.exs
+++ apps/api_gateway/test/controllers/v1/handler_controller_test.exs
@@ -3,62 +3,27 @@ defmodule ApiGateway.V1.HandlerControllerTest do
 
   test "GET :index", %{conn: conn} do
     Application.get_all_env(:handler)
-    |> Enum.each(
-         fn {url, handlers} ->
-           case handlers do
-             %{index: _} -> conn_index(conn, url)
-             _ -> nil
-           end
-         end
-       )
+    |> Enum.each(&(index_handler(conn, &1)))
   end
 
   test "POST :create", %{conn: conn} do
     Application.get_all_env(:handler)
-    |> Enum.each(
-         fn {url, handlers} ->
-           case handlers do
-             %{create: _} -> conn_create(conn, url)
-             _ -> nil
-           end
-         end
-       )
+    |> Enum.each(&(create_handler(conn, &1)))
   end
 
   test "PUT :update", %{conn: conn} do
     Application.get_all_env(:handler)
-    |> Enum.each(
-         fn {url, handlers} ->
-           case handlers do
-             %{update: _} -> conn_update(conn, url)
-             _ -> nil
-           end
-         end
-       )
+    |> Enum.each(&(update_handler(conn, &1)))
   end
 
   test "GET :show", %{conn: conn} do
     Application.get_all_env(:handler)
-    |> Enum.each(
-         fn {url, handlers} ->
-           case handlers do
-             %{show: _} -> conn_show(conn, url)
-             _ -> nil
-           end
-         end
-       )
+    |> Enum.each(&(show_handler(conn, &1)))
   end
 
   test "DELETE :delete", %{conn: conn} do
     Application.get_all_env(:handler)
-    |> Enum.each(
-         fn {url, handlers} ->
-           case handlers do
-             %{delete: _} -> conn_delete(conn, url)
-             _ -> nil
-           end
-         end
-       )
+    |> Enum.each(&(delete_handler(conn, &1)))
   end
 
   defp conn_index(conn, url) do
@@ -66,6 +31,13 @@ defmodule ApiGateway.V1.HandlerControllerTest do
                |> get(v1_handler_path(conn, :index, url))
                |> json_response(200)
     assert %{"data" => _} = response
+  end
+
+  defp index_handler(conn, {url, handlers}) do
+    case handlers do
+      %{index: _} -> conn_index(conn, url)
+      _ -> nil
+    end
   end
 
   defp conn_create(conn, url) do
@@ -83,6 +55,13 @@ defmodule ApiGateway.V1.HandlerControllerTest do
                |> post(path)
                |> json_response(200)
     assert %{"error" => _} = response
+  end
+
+  defp create_handler(conn, {url, handlers}) do
+    case handlers do
+      %{create: _} -> conn_create(conn, url)
+      _ -> nil
+    end
   end
 
   defp conn_update(conn, url) do
@@ -105,6 +84,28 @@ defmodule ApiGateway.V1.HandlerControllerTest do
     assert %{"error" => _} = response
   end
 
+  defp update_handler(conn, {url, handlers}) do
+    case handlers do
+      %{update: _} -> conn_update(conn, url)
+      _ -> nil
+    end
+  end
+
+  defp conn_show(conn, url) do
+    response =
+      conn
+      |> get(v1_handler_path(conn, :show, url, 1))
+      |> json_response(200)
+    assert %{"data" => _} = response
+  end
+
+  defp show_handler(conn, {url, handlers}) do
+    case handlers do
+      %{show: _} -> conn_show(conn, url)
+      _ -> nil
+    end
+  end
+
   defp conn_delete(conn, url) do
 
     path = v1_handler_path(
@@ -121,12 +122,11 @@ defmodule ApiGateway.V1.HandlerControllerTest do
     assert %{"data" => _} = response
   end
 
-  defp conn_show(conn, url) do
-    response =
-      conn
-      |> get(v1_handler_path(conn, :show, url, 1))
-      |> json_response(200)
-    assert %{"data" => _} = response
+  defp delete_handler(conn, {url, handlers}) do
+    case handlers do
+      %{delete: _} -> conn_delete(conn, url)
+      _ -> nil
+    end
   end
 
 end
